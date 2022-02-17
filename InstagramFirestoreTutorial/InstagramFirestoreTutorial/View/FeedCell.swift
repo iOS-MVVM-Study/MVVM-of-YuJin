3a8feb31213bd8693db995e32dc27ef1a24da16a
Yujin Lee <yj587878@gmail.com>
2022-02-17 21:31:19 +0900

    [FEAT] FeedCell 구현

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
@@ -0,0 +1,146 @@
+//
+//  FeedCell.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 이유진 on 2022/02/17.
+//
+
+import UIKit
+
+import UIKit
+
+class FeedCell: UICollectionViewCell{
+    
+    //MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.image = ImageLiterals.venom7
+        return imageView
+    }()
+    
+    private lazy var userNameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .bold)
+        button.addTarget(self, action: #selector(didTapUserName), for: .touchUpInside)
+        return button
+    }()
+    
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.image = ImageLiterals.venom7
+        return imageView
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(ImageLiterals.icLikeUnselected, for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(ImageLiterals.icComment, for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(ImageLiterals.icSend, for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private let likeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = .systemFont(ofSize: 13, weight: .bold)
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some test caption for now"
+        label.font = .systemFont(ofSize: 12, weight: .bold)
+        return label
+    }()
+    
+    private let postTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 days ago"
+        label.font = .systemFont(ofSize: 12, weight: .bold)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    //MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("FeedCell init")
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    //MARK: - Actions
+    
+    @objc
+    func didTapUserName(){
+        print("DEBUG: didTapUserName")
+    }
+    
+    //MARK: - Helper
+    
+    func configureUI(){
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor,
+                                paddingTop: 12, paddingLeft: 12)
+        profileImageView.setDimensions(height: 40, width: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(userNameButton)
+        userNameButton.centerY(inView: profileImageView,
+                               leftAnchor: profileImageView.rightAnchor,
+                               paddingLeft: 8)
+        
+        addSubview(postImageView)
+        postImageView.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8)
+        
+        postImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        configureActionButtons()
+        
+        addSubview(likeLabel)
+        likeLabel.anchor(top: likeButton.bottomAnchor, left: leftAnchor,
+                         paddingTop: -4, paddingLeft: 8)
+        
+        addSubview(captionLabel)
+        captionLabel.anchor(top: likeLabel.bottomAnchor, left: leftAnchor,
+                         paddingTop: 8, paddingLeft: 8)
+        
+        addSubview(postTimeLabel)
+        postTimeLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor,
+                         paddingTop: 8, paddingLeft: 8)
+        
+    }
+    
+    private func configureActionButtons(){
+        let stackView = UIStackView(arrangedSubviews: [likeButton, commentButton, shareButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        stackView.anchor(top: postImageView.bottomAnchor, width: 120, height: 50)
+    }
+}
