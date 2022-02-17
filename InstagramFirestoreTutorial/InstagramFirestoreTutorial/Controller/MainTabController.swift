da3b34dcc8f60b9b76ba8f78f0489d54b94e2300
Yujin Lee <yj587878@gmail.com>
2022-02-17 20:06:27 +0900

    [CHORE] 프로젝트 폴더링 및 루트뷰 설정

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 이유진 on 2022/02/17.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
