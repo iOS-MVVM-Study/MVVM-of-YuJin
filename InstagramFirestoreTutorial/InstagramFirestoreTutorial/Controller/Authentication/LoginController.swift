d09d0dd5849fbd7575ebc57e48b28baac91c2d57
Yujin Lee <yj587878@gmail.com>
2022-02-24 04:30:20 +0900

    [ADD] LoginVC,RegisterVC 추가 및 RootVC 변경

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 이유진 on 2022/02/24.
+//
+
+import UIKit
+
+
+import UIKit
+
+import UIKit
+
+class LoginController: UIViewController{
+
+    //MARK: - Properties
+    
+    //MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 이유진 on 2022/02/24.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController{
+
+    //MARK: - Properties
+    
+    //MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
