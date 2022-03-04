f54f1ced24f54710a4c94e19c9d8f7221688fe6c
Yujin Lee <yj587878@gmail.com>
2022-03-04 20:54:59 +0900

    [FEAT] Profile 탭 Header 구현

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/Profile/ProfileCell.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/Profile/ProfileCell.swift
@@ -11,12 +11,22 @@ class ProfileCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .lightGray
+        
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder){
